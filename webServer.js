90d1f4b2b21f4e6c483319f6cc27190b69894f32
gsahithi7 <123212347+gsahithi7@users.noreply.github.com>
2024-02-23 22:41:41 +0000

    Added login api

--- webServer.js
+++ webServer.js
@@ -4,8 +4,14 @@ const port = 3000;
 const cors = require('cors');
 const fs = require('fs'); // Import the fs module
 const path = require('path'); // Node.js module for working with file paths
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const secretKey = "Express is best!";
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
 
 // Route to serve the index.html file
 app.get('/', (req, res) => {
@@ -19,7 +25,40 @@ app.listen(port, () => {
 
 const userData = JSON.parse(fs.readFileSync('user_credentials.json'));
 
-app.get('/users', (req, res) => {
-    res.json(userData);
-});
 
+
+
+app.get('/auth/login',(req,res)=>{
+
+    const {username,password} = req.body;
+   
+    console.log(req);
+    if(!username || !password){
+        return res.status(400).json({message: "Username and password are required for logging in!" });
+    }
+
+    const user = userData.find(u => { if( toString(u.username).toLowerCase() == toString(username).toLocaleLowerCase() && u.password == password) return u ;else return null; } );
+    if(user){
+    
+        let generatedToken = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '3m' });
+    
+    res.status(200).json({
+        success:true,
+        error:false,
+        token : generatedToken,
+        msg: username+" authenticated successfully!!"
+    });
+
+   }else{
+
+    res.status(400).json({
+        success:false,
+        error:true,
+        token:null,
+       msg:"Username or password is incorrect!"
+    });
+
+   }
+    
+
+});
